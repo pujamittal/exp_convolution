a20d1b91646713c19114c6c7ea118be0283d9f64
pujamittal <mittal21@purdue.edu>
2018-10-24 14:22:25 -0400

    user input convolving with random numbers

--- convolution_v0.py
+++ convolution_v0.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 #get user input
 #user input has two fields
@@ -10,24 +11,25 @@ a1 = []
 a2 = []
 
 while True:
-    arr = raw_input("which array are you inserting into? ")
+    #arr = raw_input("which array are you inserting into? ")
     val = input("what value? ")
 
     # print(arr)
     # print(val)
 
-    if arr == "a1":
-        a1.append(val)
-    if arr == "a2":
-        a2.append(val)
+    #if arr == "a1":
+    a1.append(val)
+    # if arr == "a2":
+    #     a2.append(val)
+
+    #need to append a number to the second array as well. both need to be the same size in order to produce a single value
+    a2.append(random.randint(0,100)) #in the operational code, we would read one number at a time from the data set then loop back once we hit the size
 
     # print(a1)
     # print(a2)
 
     if a1 and a2:
-        c = np.convolve(a1,a2,'valid')
+        c = np.convolve(a1,a2,'valid') #look at https://docs.scipy.org/doc/numpy/reference/generated/numpy.convolve.html
         print(c)
 
-
-    #change a2 to generate a random number from 0-255 every time we insert into a1 to convolve with
-    #in the operational code, we would read one number at a time from the data set then loop back once we hit the size
+    
