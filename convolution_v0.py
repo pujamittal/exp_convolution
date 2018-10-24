5e5a025d560eb1e2ce42d74846fd004d6b82f4aa
pujamittal <mittal21@purdue.edu>
2018-10-24 14:29:19 -0400

    convolving with nums 1 through 100

--- convolution_v0.py
+++ convolution_v0.py
@@ -9,21 +9,23 @@ import random
 
 a1 = []
 a2 = []
+data = range(1,100)
+index = 0
+
 
 while True:
-    #arr = raw_input("which array are you inserting into? ")
     val = input("what value? ")
-
-    # print(arr)
-    # print(val)
-
-    #if arr == "a1":
     a1.append(val)
-    # if arr == "a2":
-    #     a2.append(val)
 
-    #need to append a number to the second array as well. both need to be the same size in order to produce a single value
-    a2.append(random.randint(0,100)) #in the operational code, we would read one number at a time from the data set then loop back once we hit the size
+    #v1: convolving with random integer
+    #   need to append a number to the second array as well. both need to be the same size in order to produce a single value
+    #a2.append(random.randint(0,100)) #in the operational code, we would read one number at a time from the data set then loop back once we hit the size
+
+    #v2: convolving with 1 - 100
+    a2.append(data[index])
+    index += 1
+    if index == 99: #loop back to the start
+        index = 1;
 
     # print(a1)
     # print(a2)
